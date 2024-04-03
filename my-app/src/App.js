5f7c28e5a62e524ffe5c5fabd513daf265e2c5fc
douglas <douglasmaxton@gmail.com>
2024-04-03 15:53:11 +0100

    added: 3 anchor tags to App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-    </div>
-  );
+    return (
+        <div className={css.App}>
+            <div>
+                <a href="">anchor 1</a><a href="">anchor 2</a><a href="">anchor 3</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
